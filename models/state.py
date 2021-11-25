efa842baaf0461d4858e08d911c7b78d2bb1f659
1107tomkim <cryptotaekim@gmail.com>
2021-11-25 04:00:33 +0000

    Changing the fixes for state model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import os
+import models
+import models.city import city
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+
+    else:
+        @property
+        def cities(self):
+            """"""
+            list_cities = []
+            for item in models.storage.all(City).values():
+                if item.state_id == self.id:
+                    list_cities.append(item)
+            return list_cities
